7be1ff708777d7bebd80db65df0b8946c990d629
shinuhyun <shinuhyun@gmail.com>
2021-08-16 14:42:22 +0900

    JS: Fix navbar

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    if (window.pageYOffset > navbarHeight) {
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
