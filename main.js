abdb64d6809dfa2d829141862ead3d2441e8ab12
shinuhyun <shinuhyun@gmail.com>
2021-08-17 12:45:23 +0900

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -15,16 +15,20 @@ document.addEventListener('scroll', () => {
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
-    console.log(target)
-    target.style.border = '1px solid var(--color-white);';
-
     const link = target.dataset.link;
     if(link == null) {
         return;
     }
-    
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 })
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+})
+
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
